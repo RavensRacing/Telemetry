cc1a65456bd1562d4e0ff56c7560e91902bb07db
Adrian Joaquin <60612052+adrianjoaquin2003@users.noreply.github.com>
2025-02-07 10:52:31 -0500

    Built venv and added requirements

--- receiver_side/build_csv.py
+++ receiver_side/build_csv.py
@@ -1,37 +1,14 @@
-import csv
-import matplotlib.pyplot as plt
+import serial
 
-def plot_line(csv_path: str):
+source = serial.Serial('/dev/ttyACM0', 9600, timeout = .1)
 
-    with open(csv_path) as file:
+lapping = True
+lines = list()
 
-        all_data = csv.DictReader(file)
+while lapping:
+    data = source.readline()
 
-        pos_x = list()
-        pos_y = list()
+    if data:
+        lines.append(str(data))
 
-        cur_pos_x = 0
-        cur_pos_y = 0
-        cur_time = 0
-
-        for i in all_data:
-            time = float(i['time'])
-            vx = float(i['vx'])
-            vy = float(i['vy'])
-
-            delta_t = time - cur_time
-
-            new_x = cur_pos_x + vx * delta_t
-            new_y = cur_pos_y + vy * delta_t
-
-            pos_x.append(new_x)
-            pos_y.append(new_y)
-
-            cur_time = time
-            cur_pos_x = new_x
-            cur_pos_y = new_y
-
-        
-        fig, ax = plt.subplots()
-        ax.plot(pos_x, pos_y)
-        plt.show()
+# Add processing logic to build csv based on output from telemetry
