382cbc255db9fa230b35b352e4f3081a839d792c
Adrian Joaquin <60612052+adrianjoaquin2003@users.noreply.github.com>
2025-02-07 17:47:39 -0500

    got it working!

--- receiver_side/build_csv.py
+++ receiver_side/build_csv.py
@@ -1,5 +1,7 @@
 import serial
+from plot_race_line import plot_line
 
+#Must be updated based on computer running program.
 source = serial.Serial('/dev/ttyACM0', 9600, timeout = .1)
 
 lapping = True
@@ -11,4 +13,9 @@ while lapping:
     if data:
         lines.append(str(data))
 
+print("Lap complete\nWriting logs...")
 # Add processing logic to build csv based on output from telemetry
+
+print("Logging complete\nCreating visualization...")
+
+plot_line("lap.csv")
--- receiver_side/plot_race_line.py
+++ receiver_side/plot_race_line.py
@@ -1,56 +1,38 @@
 import matplotlib.pyplot as plt
 import csv
+import geopy.distance
+import math
 
 def find_position(cur_pos, velocity, delta_t):
     return cur_pos + velocity * delta_t  # Basic position update formula
 
-def get_color(throttle_pos, brake_pos):
+def get_color(throttle_pos):
     if throttle_pos > 0:
         return (1, 0, 0, throttle_pos / 100)  # Red intensity based on throttle
-    elif brake_pos > 0:
-        return (0, 0, 1, brake_pos / 100)  # Blue intensity based on brake
     else:
         return (0.5, 0.5, 0.5, 1)  # Grey for coasting
 
 def plot_line(csv_path: str):
     with open(csv_path, newline='') as file:
-        all_data = list(csv.DictReader(file))  # Convert to list
 
-    cur_pos_x = 0
-    cur_pos_y = 0
-    cur_time = 0
+        all_data = list(csv.DictReader(file))
 
-    fig, ax = plt.subplots()
+        pos_lat = list()
+        pos_long = list()
 
-    # Extract each of the datapoints from each time frame
-    for i in range(len(all_data)):
-        time = float(all_data[i]['time'])
-        vx = float(all_data[i]['vx'])
-        vy = float(all_data[i]['vy'])
-        throttle_pos = float(all_data[i]['throttle_pos'])
-        brake_pos = float(all_data[i]['brake_pos'])
+        fig, ax = plt.subplots()
 
-        # Update time interval between time frames
-        delta_t = time - cur_time
-        cur_time = time
+        for i in range(len(all_data) - 1):
+        
+            lat = float(all_data[i]['lat'])
+            long = float(all_data[i]['long'])
+            throttle_pos = float(all_data[i]['throttle'])
 
-        # Compute the new position of the car
-        new_x = find_position(cur_pos_x, vx, delta_t)
-        new_y = find_position(cur_pos_y, vy, delta_t)
+            lat_next = float(all_data[i + 1]['lat'])
+            long_next = float(all_data[i]['long'])
 
-        # Determine line color based on pedal inputs
-        color = get_color(throttle_pos, brake_pos)
+            ax.plot([lat, lat_next], [long, long_next], color=get_color(throttle_pos))
 
-        # Plot the new position of the car, apply relevant color for the time frame
-        ax.plot([cur_pos_x, new_x], [cur_pos_y, new_y], color=color, linewidth=2)
+        plt.show()
 
-        # Update position of the car
-        cur_pos_x = new_x
-        cur_pos_y = new_y
-
-    plt.xlabel("X Position")
-    plt.ylabel("Y Position")
-    plt.title("Racing Line with Throttle/Brake Intensity")
-    plt.show()
-
-plot_line("sample_lap.csv")
+plot_line("sample_lap - UofT-GPS-velo.csv")
