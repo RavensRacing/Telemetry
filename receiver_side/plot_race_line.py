8f95309136587c18d40316d24c8b9169074c1ed3
Adrian Joaquin <60612052+adrianjoaquin2003@users.noreply.github.com>
2025-02-07 14:21:53 -0500

    Added color code based on pedal input feature
    Racing line is colored differently based on the position of each throttle as sent over telemetry

--- receiver_side/plot_race_line.py
+++ receiver_side/plot_race_line.py
@@ -1,37 +1,56 @@
-import csv
 import matplotlib.pyplot as plt
+import csv
+
+def find_position(cur_pos, velocity, delta_t):
+    return cur_pos + velocity * delta_t  # Basic position update formula
+
+def get_color(throttle_pos, brake_pos):
+    if throttle_pos > 0:
+        return (1, 0, 0, throttle_pos / 100)  # Red intensity based on throttle
+    elif brake_pos > 0:
+        return (0, 0, 1, brake_pos / 100)  # Blue intensity based on brake
+    else:
+        return (0.5, 0.5, 0.5, 1)  # Grey for coasting
 
 def plot_line(csv_path: str):
+    with open(csv_path, newline='') as file:
+        all_data = list(csv.DictReader(file))  # Convert to list
 
-    with open(csv_path) as file:
+    cur_pos_x = 0
+    cur_pos_y = 0
+    cur_time = 0
 
-        all_data = csv.DictReader(file)
+    fig, ax = plt.subplots()
 
-        pos_x = list()
-        pos_y = list()
+    # Extract each of the datapoints from each time frame
+    for i in range(len(all_data)):
+        time = float(all_data[i]['time'])
+        vx = float(all_data[i]['vx'])
+        vy = float(all_data[i]['vy'])
+        throttle_pos = float(all_data[i]['throttle_pos'])
+        brake_pos = float(all_data[i]['brake_pos'])
 
-        cur_pos_x = 0
-        cur_pos_y = 0
-        cur_time = 0
+        # Update time interval between time frames
+        delta_t = time - cur_time
+        cur_time = time
 
-        for i in all_data:
-            time = float(i['time'])
-            vx = float(i['vx'])
-            vy = float(i['vy'])
+        # Compute the new position of the car
+        new_x = find_position(cur_pos_x, vx, delta_t)
+        new_y = find_position(cur_pos_y, vy, delta_t)
 
-            delta_t = time - cur_time
+        # Determine line color based on pedal inputs
+        color = get_color(throttle_pos, brake_pos)
 
-            new_x = cur_pos_x + vx * delta_t
-            new_y = cur_pos_y + vy * delta_t
+        # Plot the new position of the car, apply relevant color for the time frame
+        ax.plot([cur_pos_x, new_x], [cur_pos_y, new_y], color=color, linewidth=2)
 
-            pos_x.append(new_x)
-            pos_y.append(new_y)
+        # Update position of the car
+        cur_pos_x = new_x
+        cur_pos_y = new_y
 
-            cur_time = time
-            cur_pos_x = new_x
-            cur_pos_y = new_y
+    plt.xlabel("X Position")
+    plt.ylabel("Y Position")
+    plt.title("Racing Line with Throttle/Brake Intensity")
+    plt.show()
 
-        
-        fig, ax = plt.subplots()
-        ax.plot(pos_x, pos_y)
-        plt.show()
+plot_line("sample_lap.csv")
